a2cd7a7ab79081b4b858c60af1b99b72b0c9f70c
Khairul-Basar <khairulbasar130@gmail.com>
2022-03-14 11:36:36 +0600

    added vector icon

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import {ImageBackground, StyleSheet} from 'react-native';
+import React from 'react'
+import { ImageBackground, StyleSheet, TouchableOpacity } from 'react-native'
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
-} from 'react-native-responsive-screen';
-import AddTeam from './src/screens/AddTeam/AddTeam';
-import BickTeam from './src/screens/BickTeam/BickTeam';
+} from 'react-native-responsive-screen'
+import { Icon } from 'react-native-elements'
+
+import AddTeam from './src/screens/AddTeam/AddTeam'
+import BickTeam from './src/screens/BickTeam/BickTeam'
 
 export default function App() {
   return (
     <ImageBackground
       source={require('./assets/backgroundSoccer.png')}
-      style={styles.container}>
-      {/* <AddTeam></AddTeam> */}
-      <BickTeam></BickTeam>
+      style={styles.container}
+    >
+      <TouchableOpacity onPress={() => console.log('a')}>
+        <Icon name="heart" type="font-awesome" color="white" size={54} />
+      </TouchableOpacity>
     </ImageBackground>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: wp('7%'),
-    paddingHorizontal: wp('3%'),
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+})
